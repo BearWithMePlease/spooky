bda92c146cc86b28a1546d2bb46dd5eaf0d12dcb
BearWithMe <happyhammers513@gmail.com>
2024-09-12 12:18:55 +0200

    player

--- scripts/player/bullet.gd
+++ scripts/player/bullet.gd
@@ -1,6 +1,5 @@
-extends RigidBody2D
+extends Sprite2D
 
-class_name Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	move_local_x(5000*delta)
 	pass
