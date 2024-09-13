a2fe87d030e0277dcb2f45a5e54d368334d6805d
Nenthor <david@nentwich.dev>
2024-09-13 13:46:48 +0200

    Added module limit cap

--- scripts/module_button.gd
+++ scripts/module_button.gd
@@ -1,6 +1,9 @@
 extends Button
+class_name ModuleButton
 
 @export var module_type: Modules.ModuleType = Modules.ModuleType.NONE
+
+var _placeable_count: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +12,19 @@ func _ready() -> void:
 		return
 		
 	var path = Modules.get_icon_path(module_type)
-	$"./Module_Img".texture = load(path)
+	$"Module_Img".texture = load(path)
+	
+	set_placeable_count(Modules.max_module_count[self.module_type])
 	
 	var callable = Callable($"..".on_module_type_select).bind(module_type)
 	self.pressed.connect(callable)
 	
+
+func set_placeable_count(new_count: int):
+	self._placeable_count = clampi(new_count, 0, Modules.max_module_count[self.module_type])
+	$Count.text = str(self._placeable_count)
+	
+	self.disabled = self._placeable_count == 0
+
+func add_to_placeable_count(value: int):
+	set_placeable_count(self._placeable_count + value)
