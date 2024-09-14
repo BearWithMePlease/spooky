053c772e74cb6f653396ba1252d6a24f34a1eb86
Nenthor <david@nentwich.dev>
2024-09-15 00:38:14 +0200

    Making of start menu

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,11 +1,13 @@
 extends Node
 
 enum SceneType {
+	START_MENU,
 	BUNKER_BUILD,
 	MAIN_SCENE
 }
 
 var packed_scenes := {
+	SceneType.START_MENU: preload("res://scenes/start_menu.tscn"),
 	SceneType.BUNKER_BUILD: preload("res://scenes/bunker_builder.tscn"),
 	SceneType.MAIN_SCENE: preload("res://scenes/main_scene.tscn")
 }
