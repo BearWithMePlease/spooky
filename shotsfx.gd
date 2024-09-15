d6ec11e3bf55f4f3854a08324b49b5c81a71a487
BearWithMe <happyhammers513@gmail.com>
2024-09-15 02:18:36 +0200

    abacada

--- shotsfx.gd
+++ shotsfx.gd
@@ -3,7 +3,6 @@ extends AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play()
 	print(volume_db)
 	await get_tree().create_timer(1).timeout
 	queue_free()
